87421c921c41b874ff6f2625cdd6716579d0971b
Stefano Vazzoler <github@stefanovazzoler.com>
2023-11-13 17:30:54 -0800

    Improve control flow test

--- bench/controlFlow_test.go
+++ bench/controlFlow_test.go
@@ -47,26 +47,37 @@ func BenchmarkControlFlowComplex(b *testing.B) {
 			x = i % 11
 			switch x {
 			case 0:
+				_ = 0
 				continue
 			case 1:
+				_ = 0
 				continue
 			case 2:
+				_ = 0
 				continue
 			case 3:
+				_ = 0
 				continue
 			case 4:
+				_ = 0
 				continue
 			case 5:
+				_ = 0
 				continue
 			case 6:
+				_ = 0
 				continue
 			case 7:
+				_ = 0
 				continue
 			case 8:
+				_ = 0
 				continue
 			case 9:
+				_ = 0
 				continue
 			default:
+				_ = 0
 				continue
 			}
 		}
@@ -75,35 +86,46 @@ func BenchmarkControlFlowComplex(b *testing.B) {
 		for i := 0; i < b.N; i++ {
 			x = i % 11
 			if x == 0 {
+				_ = 0
 				continue
 			}
 			if x == 1 {
+				_ = 0
 				continue
 			}
 			if x == 2 {
+				_ = 0
 				continue
 			}
 			if x == 3 {
+				_ = 0
 				continue
 			}
 			if x == 4 {
+				_ = 0
 				continue
 			}
 			if x == 5 {
+				_ = 0
 				continue
 			}
 			if x == 6 {
+				_ = 0
 				continue
 			}
 			if x == 7 {
+				_ = 0
 				continue
 			}
 			if x == 8 {
+				_ = 0
 				continue
 			}
 			if x == 9 {
+				_ = 0
 				continue
 			}
+			_ = 0
 			continue
 		}
 	})
@@ -111,27 +133,27 @@ func BenchmarkControlFlowComplex(b *testing.B) {
 		for i := 0; i < b.N; i++ {
 			x = i % 11
 			if x == 0 {
-				continue
+				_ = 0
 			} else if x == 1 {
-				continue
+				_ = 0
 			} else if x == 2 {
-				continue
+				_ = 0
 			} else if x == 3 {
-				continue
+				_ = 0
 			} else if x == 4 {
-				continue
+				_ = 0
 			} else if x == 5 {
-				continue
+				_ = 0
 			} else if x == 6 {
-				continue
+				_ = 0
 			} else if x == 7 {
-				continue
+				_ = 0
 			} else if x == 8 {
-				continue
+				_ = 0
 			} else if x == 9 {
-				continue
+				_ = 0
 			} else {
-				continue
+				_ = 0
 			}
 		}
 	})
